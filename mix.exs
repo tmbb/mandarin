564f11c39cf00b8b5bede3e6e05e8fdb0478a26a
tmbb <tmbb@campus.ul.pt>
2018-11-22 18:58:14 +0000

    add dependencies (needs override in ecto untill paginator supports ecto 3.0)

--- mix.exs
+++ mix.exs
@@ -21,8 +21,9 @@ defmodule Bureaucrat.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:forage, git: "https://github.com/tmbb/forage.git"},
+      {:ecto, "~> 3.0", override: true},
+      {:ecto_sql, "~> 3.0", override: true}
     ]
   end
 end
