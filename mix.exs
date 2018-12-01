4bff3a564f2249918ec71f2d8b966c1fe0ed89bb
tmbb <tmbb@campus.ul.pt>
2018-12-01 13:38:01 +0000

    publsh on hex

--- mix.exs
+++ mix.exs
@@ -1,12 +1,16 @@
 defmodule Mandarin.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :mandarin,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
@@ -22,9 +26,26 @@ defmodule Mandarin.MixProject do
   defp deps do
     [
       {:phoenix, "~> 1.4"},
-      {:forage, git: "https://github.com/tmbb/forage.git"},
-      {:ecto, "~> 3.0", override: true},
-      {:ecto_sql, "~> 3.0", override: true}
+      {:forage, "~> 0.1"},
+      {:ecto, "~> 3.0"},
+      {:ecto_sql, "~> 3.0"},
+      {:ex_doc, "~> 0.19", only: :dev}
+    ]
+  end
+
+  defp description() do
+    "Admin interface generator"
+  end
+
+  defp package() do
+    [
+      # This option is only needed when you don't want to use the OTP application name
+      name: "mandarin",
+      # These are the default files included in the package
+      files: ~w(lib priv .formatter.exs mix.exs README* readme* LICENSE*
+                license* CHANGELOG* changelog* src),
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/tmbb/mandarin"}
     ]
   end
 end
