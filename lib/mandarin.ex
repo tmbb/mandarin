7de5e19e309939ffc33a1b1d553f03a9cbee40b9
Patrick De Marta <patrick.demarta@gmail.com>
2019-06-18 23:59:13 +0200

    Update mandarin.ex
    fix typos

--- lib/mandarin.ex
+++ lib/mandarin.ex
@@ -3,12 +3,12 @@ defmodule Mandarin do
   A package containing generators to generate an admin backend for your Phoenix app.
 
   The package relies a lot on generators, and Mandarin itself provides a minimal API at runtime.
-  This is be design, as the code produced with generators is easier to customize than overridable
+  This is by design, as the code produced with generators is easier to customize than overridable
   API calls, or other alternatives that depend a lot on macros.
 
-  The basic idea is tha Mandarin generates normal Phoenix contexts, views, templates and controllers,
+  The basic idea is that Mandarin generates normal Phoenix contexts, views, templates and controllers,
   much like the ones you'd write yourself.
-  The generated files follow roughly the same conventinos as the ones you'd write yourself.
+  The generated files follow roughly the same conventions as the ones you'd write yourself.
 
   Mandarin relies heavily on the [forage](https://github.com/tmbb/forage) package
   for filtering, sorting and paginating the results in the CRUD views.
